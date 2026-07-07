4ae050b04e3427b09d53d0e64953f2fc7238b61c
Davi Altafim Santana <davialtafimsantana@gmail.com>
2026-07-06 21:15:10 -0300

    fix: adiciona importação faltante do DeleteMapping

--- src/main/java/com/ProjetoIntegrado/DependancyHeaven/web/TemplateController.java
+++ src/main/java/com/ProjetoIntegrado/DependancyHeaven/web/TemplateController.java
@@ -7,6 +7,7 @@ import com.ProjetoIntegrado.DependancyHeaven.service.TemplateService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
